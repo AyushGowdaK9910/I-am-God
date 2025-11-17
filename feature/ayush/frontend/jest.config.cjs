562547b1f3ee30f3124da8633ee9d4f003b3509c
AyushGowdaK9910 <ayushgowdak9910@gmail.com>
2025-11-18 00:47:52 +0530

    chore: update frontend jest config with 80% coverage thresholds

--- feature/ayush/frontend/jest.config.cjs
+++ feature/ayush/frontend/jest.config.cjs
@@ -9,6 +9,14 @@ module.exports = {
     '!src/main.tsx',
   ],
   coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
   setupFilesAfterEnv: ['<rootDir>/src/__tests__/setup.ts'],
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
